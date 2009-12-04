a63c1fb9923156fdc0cdbd0f33fdcd53c7d84085
Oleg Shnitko <olegshnitko@oleg-shnitkos-mac-mini.local>
2009-12-04 18:26:51 +0200

    Open links in system default browser

--- Classes/Controllers/WebViewController.m
+++ Classes/Controllers/WebViewController.m
@@ -21,4 +21,22 @@
     [[_webView mainFrame] loadHTMLString:content baseURL:nil];
 }
 
+- (void)webView:(WebView *)sender decidePolicyForNavigationAction:(NSDictionary *)actionInformation
+        request:(NSURLRequest *)request
+          frame:(WebFrame *)frame
+decisionListener:(id <WebPolicyDecisionListener>)listener
+{
+    NSInteger type = [[actionInformation objectForKey:WebActionNavigationTypeKey] intValue];
+    
+    if (type == WebNavigationTypeLinkClicked)
+    {
+        [listener ignore];
+        [[NSWorkspace sharedWorkspace] openURL:[request URL]];
+    }
+    else
+    {
+        [listener use];
+    }
+}
+
 @end
